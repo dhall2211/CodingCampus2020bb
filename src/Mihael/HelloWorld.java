7918acf0e1e3154ec55c80c10acbf97029507f00
Mihae1 <michaelrpankin@gmail.com>
2020-12-03 21:24:04 +0100

    2x ausschreiben das HelloWorld

--- src/Mihael/HelloWorld.java
+++ src/Mihael/HelloWorld.java
@@ -3,5 +3,6 @@ package Mihael;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
