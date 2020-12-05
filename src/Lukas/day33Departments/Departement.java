159e20f53002515b41c0eb6974eb6b0b9339cdb4
tiveritz <lukas.walch@icloud.com>
2020-12-05 13:19:35 +0100

    Improved solution

---
+++ src/Lukas/day33Departments/Departement.java
@@ -0,0 +1,45 @@
+package Lukas.day33Departments;
+
+import java.util.Vector;
+
+public class Departement {
+	String depName;
+	String boss;
+	private Departement topDepartement;
+	private Vector<Departement> subDepartement;
+	
+	Departement(String depName, String boss, Departement topDepartement) {
+		this.depName = depName;
+		this.boss = boss;
+		subDepartement = new Vector<>();
+
+        if (topDepartement != null){
+            topDepartement.subDepartement.add(this);
+        }
+	}
+
+	public Departement(String depName, String boss) {
+        this(depName, boss, null);
+	}
+	
+	public String getOrganigram() {
+		return getOrganigram(0);
+	}
+
+	public String getOrganigram(int indentation) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < indentation; i++) {
+			sb.append(" ");
+		}
+
+		sb.append("L ").append(depName).append(" ").append(boss).append("\n");
+
+		if (this.subDepartement != null) {
+			for (int dep = 0; dep < subDepartement.size(); dep++) {
+				sb.append(subDepartement.get(dep).getOrganigram(indentation + 2));
+			}
+		}
+		return sb.toString();
+	}
+}
