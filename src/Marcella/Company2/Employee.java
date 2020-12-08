361d23c6305af9443daa5da9f7c47a78fd40cccc
marcella-merholz <marcella_merholz@web.de>
2020-12-08 20:00:08 +0100

    Aufruf addCurrentEmployee con Klasse Employee aus

--- src/Marcella/Company2/Employee.java
+++ src/Marcella/Company2/Employee.java
@@ -3,19 +3,31 @@ package Marcella.Company2;
 public class Employee {
 
     private String nameEmployee;
-    private String departementEmployee;
+    private Departement departementEmployee;
 
-    public Employee(String nameEmployee, String departementEmployee) {
+    public Employee(String nameEmployee, Departement departementEmployee) {
         this.nameEmployee = nameEmployee;
         this.departementEmployee = departementEmployee;
+        if (departementEmployee != null){
+            departementEmployee.addCurrentEmployee(this);
+        }
     }
 
     @Override
     public String toString() {
-        return nameEmployee + ", " + departementEmployee;
+        return nameEmployee;
     }
 
-    public String getDepartementEmployee() {
-        return departementEmployee;
+    public void quit() {
+        departementEmployee.removeCurrentEmployee(this);
+        departementEmployee = null;
     }
+
+    public void switchDepartment(Departement newDepartement) {
+        departementEmployee.removeCurrentEmployee(this);
+        if (newDepartement != null){
+            newDepartement.addCurrentEmployee(this);
+        }
+    }
+
 }
