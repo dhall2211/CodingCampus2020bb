d04242b9f5f35a3a68099315d853e03d2942179e
iknap <irene.knap@gmx.at>
2020-12-09 20:24:47 +0100

    additional tasks of the company example

--- src/Irene/company/Departement.java
+++ src/Irene/company/Departement.java
@@ -1,46 +1,78 @@
 package Irene.company;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Departement {
-    private String nameManager;
-    private String nameDepartement;
+    private String manager;
+    private String departement;
     private Departement parentDepartement;
     private Vector<Departement> childDepartement;
-    private Vector<Departement> persons;
+    private ArrayList employees;
 
-    public Departement(String nameManager, String nameDepartement, Irene.company.Departement parentDepartement) {
-        this.nameManager = nameManager;
-        this.nameDepartement = nameDepartement;
+    public Departement(String manager, String departement, Departement parentDepartement, ArrayList employees) {
+        this.manager = manager;
+        this.departement = departement;
         this.parentDepartement = parentDepartement;
         childDepartement = new Vector<>();
-        persons = new Vector<>();
+        this.employees = new ArrayList<>();
 
         if (parentDepartement != null) {
             parentDepartement.childDepartement.add(this);
         }
-    }
 
-    public Departement(String nameManager, String nameDepartement) {
-        this(nameManager, nameDepartement, null);
-    }
-
-    public void connectToDepartment(Departement parent){
-        if (parentDepartement != null) {
-            System.out.println(nameDepartement + " ist schon eine Unterabeitlung von " + parentDepartement.nameDepartement);
-        } else {
-            parentDepartement = parent;
-            parent.childDepartement.add(this);
+        if (employees != null) {
+            for (Object employee : employees) {
+                this.employees.add(employee);
+            }
         }
+    }
+
+    public Departement(String manager, String departement) {
+        this(manager, departement, null, null);
+    }
+
+    public Departement(String manager, String departement, Departement parentDepartement) {
+        this(manager, departement, parentDepartement, null);
+    }
+
+    public void connectToDepartment(Departement parentDepartement) {
+        if (parentDepartement != null) {
+            System.out.println(departement + " is already a subdepartement of " + parentDepartement.departement);
+            System.out.println();
+        } else {
+            parentDepartement = parentDepartement;
+            parentDepartement.childDepartement.add(this);
+        }
+    }
+
+    public void switchEmployees(String employee, Departement newDepartment) {
+        this.employees.remove(employee);
+        newDepartment.employees.add(employee);
+    }
+
+    public void switchDepartment(Departement department) {
+        if (parentDepartement != null) {
+            parentDepartement.childDepartement.remove(this);
+        }
+        parentDepartement = department;
+        department.childDepartement.add(this);
+    }
+
+    public void removeDepartment() {
+        if (parentDepartement != null) {
+            parentDepartement.childDepartement.remove(this);
+        }
+        parentDepartement = null;
     }
 
     @Override
     public String toString() {
-        return "_ " + nameDepartement + ", " + nameManager + "toString";
+        return "_ " + departement + ", " + manager + ", " + employees.toString();
     }
 
     public void printCompanyOrganisation(String prefix) {
-        System.out.println(prefix + "_ " + nameDepartement + ", " + nameManager);
+        System.out.println(prefix + "_ " + departement + ", " + manager + ", " + employees.toString());
         for (int i = 0; i < childDepartement.size(); i++) {
             childDepartement.get(i).printCompanyOrganisation("    " + prefix);
         }
