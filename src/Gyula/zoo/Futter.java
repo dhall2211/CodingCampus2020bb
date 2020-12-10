9d0372b4710148022dc8ad2e17e624038d976ded
Gyula Horvath <gylhrvth@gmail.com>
2020-12-10 20:26:33 +0100

    Zoo von Gyula Version 0.2

---
+++ src/Gyula/zoo/Futter.java
@@ -0,0 +1,31 @@
+package Gyula.zoo;
+
+public class Futter {
+    private String name;
+    private String einheit;
+    private int tagesBedarf;
+
+    public Futter(Zoo zoo, String name, String einheit){
+        this.name = name;
+        this.einheit = einheit;
+        tagesBedarf = 0;
+
+        zoo.addFutter(this);
+    }
+
+    public void tagesBedarfZufuegen(int bedarf){
+        tagesBedarf += bedarf;
+    }
+
+    public void printStruktur(String prefix){
+        System.out.printf("%s%s %d %s%n", prefix, name, tagesBedarf, einheit);
+    }
+
+    public String getEinheit() {
+        return einheit;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
