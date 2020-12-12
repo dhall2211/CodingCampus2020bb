fe2a1b8c1bb89dad3c08aa8e92e920ff29142376
marcella-merholz <marcella_merholz@web.de>
2020-12-12 12:05:07 +0100

    print Structure

--- src/Marcella/Zoo/Animal.java
+++ src/Marcella/Zoo/Animal.java
@@ -4,23 +4,17 @@ public class Animal {
 
     private String name;
     private String species;
-    private Enclosure enclosure;
-    private Zoo zoo;
 
     public Animal(String name, String species, Enclosure enclosure, Zoo zoo) {
         this.name = name;
         this.species = species;
-        this.enclosure = enclosure;
-        this.zoo = zoo;
-            zoo.addEnclosure(this.enclosure);
         if (zoo != null) {
-            zoo.addAnimal(this);
+            enclosure.addAnimal(this);
         }
     }
 
-    @Override
-    public String toString() {
-        return name;
+    public void printStructure(){
+        System.out.print(this.name + ", " + species);
     }
 
 }
--- src/Marcella/Zoo/Enclosure.java
+++ src/Marcella/Zoo/Enclosure.java
@@ -1,21 +1,37 @@
 package Marcella.Zoo;
 
+import java.util.Vector;
+
 public class Enclosure {
 
     private String name;
-    private Zoo zoo;
+    private Vector <Animal> animals;
 
     public Enclosure(String name, Zoo zoo) {
         this.name = name;
-        this.zoo = zoo;
+        this.animals = new Vector<>();
         if (zoo != null) {
             zoo.addEnclosure(this);
         }
     }
 
-    @Override
-    public String toString() {
+    public String getName() {
         return name;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    public void printStructure(String prefix){
+        System.out.println(this.name);
+        System.out.print(prefix + "Tiere: ");
+        for (int i = 0; i < animals.size(); i++) {
+            animals.get(i).printStructure();
+            if (i != animals.size() -1) {
+                System.out.print(" | ");
+            }
+        }
     }
 
 }
