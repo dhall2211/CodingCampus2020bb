06dd6d1923b76a6a1d4f9e4ca602ea53f3ee14bc
Eric05 <eboesch@hotmail.com>
2020-12-17 21:12:04 +0100

    Zoo - simple simulation

--- src/Daniel/Zoo/Compound.java
+++ src/Daniel/Zoo/Compound.java
@@ -29,4 +29,31 @@ public class Compound {
     public String getName() {
         return name;
     }
+
+    /**
+     * Get food demand of (all animals in) compound.
+     * @return
+     */
+    public Vector<Demand> getDemands() {
+        var demands = new Vector<Demand>();
+        for (var animal : animalList) {
+            demands.add(animal.getDemand());
+        }
+        return demands;
+    }
+
+    public void feedAnimals(Vector<Food> foods) {
+        for (var animal : animalList) {
+            var demand = animal.getDemand();
+            Food foodToFeed = null;
+            for (var food : foods) {
+                if(food.getName().equals(demand.getFood().getName()) && food.getAmount() == demand.getAmount()){
+                    foodToFeed = food;
+                    break;
+                }
+            }
+            foods.remove(foodToFeed);
+            System.out.println("Tier " + animal.getName() + " gefüttert mit " + foodToFeed);
+        }
+    }
 }
