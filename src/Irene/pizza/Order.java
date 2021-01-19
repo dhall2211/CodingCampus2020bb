a6798ce03cea0e5174265fd4fb1dab48ca22e4c0
iknap <irene.knap@gmx.at>
2021-01-19 21:45:10 +0100

    OOP example pizza

--- src/Irene/pizza/Order.java
+++ src/Irene/pizza/Order.java
@@ -5,8 +5,15 @@ import java.util.Vector;
 public class Order {
     private Vector<Pizza> pizzaList;
     private String customerName;
+    private Vector<Topping> orderedToppingsList;
 
-    public Order(Vector<Pizza> pizzaList) {
-        this.pizzaList = pizzaList;
+    public Order(String customerName) {
+        pizzaList = new Vector<>();
+        this.customerName = customerName;
+        orderedToppingsList = new Vector<>();
+    }
+
+    public void addTopping(Topping name) {
+        orderedToppingsList.add(name);
     }
 }
--- src/Irene/pizza/Pizza.java
+++ src/Irene/pizza/Pizza.java
@@ -4,20 +4,14 @@ import java.util.Vector;
 
 public class Pizza {
     private String name;
-    private Vector<Topping> orderedToppingsList;
     private double basePrice;
 
-    public Pizza(String name, Vector<Topping> toppingsList, double basePrice) {
+    public Pizza(String name, double basePrice) {
         this.name = name;
-        this.orderedToppingsList = toppingsList;
         this.basePrice = basePrice;
     }
 
     public double getBasePrice() {
         return basePrice;
-    }
-
-    public void addTopping(Topping topping) {
-        orderedToppingsList.add(topping);
     }
 }
