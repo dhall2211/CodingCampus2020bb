af18cbe5812fe24d0d320d6db4c6e8ed3f9c464e
iknap <irene.knap@gmx.at>
2021-02-16 21:35:16 +0100

    OOP example skirental

--- src/Irene/skirental/SimulationFactory.java
+++ src/Irene/skirental/SimulationFactory.java
@@ -1,5 +1,20 @@
 package Irene.skirental;
 
-public class SimulationFactory {
+import java.util.Random;
 
+public class SimulationFactory {
+    private String[] names = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private Random random = new Random();
+
+    public SimulationFactory(){};
+
+    public Customer[] createCustomer(int count){
+        Customer[] customers = new Customer[count];
+        for (int i = 0; i < count; i++) {
+            String name = names[random.nextInt(names.length)];
+            Customer customer = new Customer(name);
+            customers[i] = customer;
+        }
+        return customers;
+    }
 }
