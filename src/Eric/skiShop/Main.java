22b47e62237e97cb1d20f4e880633472ca06c26c
Eric05 <eboesch@hotmail.com>
2021-02-16 19:57:06 +0100

    SkiShop

---
+++ src/Eric/skiShop/Main.java
@@ -0,0 +1,8 @@
+package Eric.skiShop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var items = SimulationFactory.createItems(10);
+    }
+}
---
+++ src/Eric/skiShop/Reservation.java
@@ -0,0 +1,10 @@
+package Eric.skiShop;
+
+import java.util.List;
+
+public class Reservation {
+    private List<Item> items;
+    private boolean wasCollected;
+    private int ticksSinceReservation;
+    private int ticksSincePickup;
+}
