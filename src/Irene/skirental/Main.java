cbccf0b70399f7a0cadd86c40f8e12cf231b8d46
iknap <irene.knap@gmx.at>
2021-02-18 19:57:30 +0100

    OOP example skirental

--- src/Irene/skirental/Main.java
+++ src/Irene/skirental/Main.java
@@ -1,10 +1,25 @@
 package Irene.skirental;
 
 public class Main {
-	public static void main(String[] args) {
-		Shop skiRental = Shop.getInstance();
-		WebShop skiRentalWebShop = WebShop.getInstance();
-	}
+    public static void main(String[] args) {
+        Shop skiRental = Shop.getInstance();
+        WebShop skiRentalWebShop = WebShop.getInstance();
+
+        for (Category item : Category.values()) {
+            try {
+            System.out.println(item + ": " + item.getPrice());
+            } catch (CategoryUnknownPriceException e) {
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+                //Durch das err wird die Fehlermeldung rot und am Ende ausgegeben.
+            } catch (TestException f) {
+                System.out.println(f.getMessage());
+                System.err.println(f.getMessage());
+            } finally {
+                System.out.println("This block will always run.");
+            }
+        }
+    }
 }
 
 
