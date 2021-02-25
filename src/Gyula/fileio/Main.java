bd26b0da84bdc1b275661f42cc5ee1d611c5a6ed
Gyula Horvath <gylhrvth@gmail.com>
2021-02-25 19:42:48 +0100

    File Namen Generierung

---
+++ src/Gyula/fileio/Main.java
@@ -0,0 +1,32 @@
+package Gyula.fileio;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        File myFile = null;
+        do {
+            myFile = new File(getFileName("C:\\users\\gyula.horvath\\desktop\\", "hello World", i, ".txt"));
+            System.out.println("Versuche: " + myFile.getAbsolutePath() + ": exists? " + myFile.exists());
+            ++i;
+        } while (myFile.exists());
+        System.out.println(myFile.getAbsolutePath());
+        try{
+            FileWriter fw = new FileWriter(myFile, false);
+            fw.write("Hello World!");
+            fw.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static String getFileName(String directory, String name, int counter, String extension){
+        if (counter == 1){
+            return directory + name + extension;
+        }
+        return directory + name + extension + (counter-1);
+    }
+}
